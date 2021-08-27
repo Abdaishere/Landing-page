8f5e9d4ff7b63cbb90113bb9b9f0a96c0f435dd1
Abdallah El belkasy <59124608+Abdaishere@users.noreply.github.com>
2021-08-28 00:55:27 +0200

    added navbar

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-buildSection(number, description){
+function buildSection(number, description){
     this.number = number;
     this.description = descriptionl;
 }
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
